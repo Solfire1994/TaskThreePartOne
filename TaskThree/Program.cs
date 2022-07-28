70e09f480a96e60c856c2588c2c8f456f574eb1c
solfire1994 <solfire1994@gmail.com>
2022-07-28 11:40:09 +0300

    Add project files.

---
+++ TaskThree/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TaskThree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число, и программа определит четное оно или нет");
+            string strNumber = Console.ReadLine();
+            int number = int.Parse(strNumber);
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Число {0} четное", number);
+            } else
+            {
+                Console.WriteLine("Число {0} нечетное", number);
+            }
+            Console.ReadKey();
+        }
+    }
+}
